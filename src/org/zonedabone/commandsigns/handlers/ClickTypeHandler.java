ec039071e5bb689d671f43812e952bd363e563ad
CJxD <cjdamaster@gmail.com>
2012-09-12 20:58:23 +0100

    Added << syntax
    << for left-click-only.

--- src/org/zonedabone/commandsigns/handlers/ClickTypeHandler.java
+++ src/org/zonedabone/commandsigns/handlers/ClickTypeHandler.java
@@ -7,13 +7,21 @@ public class ClickTypeHandler extends Handler {
     
     @Override
     public void handle(CommandSignExecutor e, String command, boolean silent, boolean negate) {
-        if (e.getPlayer() != null && command.startsWith(">>")) {
-            if (e.getAction() == Action.RIGHT_CLICK_BLOCK ^ negate) {
-                e.getRestrictions().push(true);
-            } else {
-                e.getRestrictions().push(false);
-            }
-        }
+    	if (e.getPlayer() != null) {
+	        if (command.startsWith(">>")) {
+	            if (e.getAction() == Action.RIGHT_CLICK_BLOCK ^ negate) {
+	                e.getRestrictions().push(true);
+	            } else {
+	                e.getRestrictions().push(false);
+	            }
+	        } else if (command.startsWith("<<")) {
+	            if (e.getAction() == Action.LEFT_CLICK_BLOCK ^ negate) {
+	                e.getRestrictions().push(true);
+	            } else {
+	                e.getRestrictions().push(false);
+	            }
+	        }
+    	}
     }
     
 }
