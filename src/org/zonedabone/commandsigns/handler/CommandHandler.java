a47ac4d84a24ac94340ebbb4de920e971b0e6b48
Kane York <rikingcoding@gmail.com>
2013-09-29 20:18:00 -0700

    import PermissionAttachment

--- src/org/zonedabone/commandsigns/handler/CommandHandler.java
+++ src/org/zonedabone/commandsigns/handler/CommandHandler.java
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.plugin.PluginManager;
 import org.zonedabone.commandsigns.CommandSigns;
 import org.zonedabone.commandsigns.SignExecutor;
